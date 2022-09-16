e1cdae7ccb7e964dad92b7d6ab078f3d2bf221c5
abin <1655853551@qq.com>
2022-09-17 00:38:00 +0800

    finish: (syscall) trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -127,6 +127,7 @@ found:
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
 
+  p->trace_mask = 0;
   return p;
 }
 
@@ -290,6 +291,8 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
+
+  np->trace_mask=p->trace_mask;
 
   pid = np->pid;
 
